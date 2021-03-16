a9c053f8315849b31720b626e2e465b26b704e5a
Nicola Galeotti <niccio2802@outlook.it>
2021-03-16 19:59:08 +0100

    Add project files.

---
+++ EsercizioTask/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EsercizioTask
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void bntEsegui_Click(object sender, RoutedEventArgs e)
+        {
+            int a = int.Parse(txtA.Text);
+            Task.Run(() => Multipli(a));
+        }
+
+        private void Multipli(int a)
+        {
+            int d = 200000000;
+            for (int i = 0; i <= d; i++)
+            {
+                if ((i % a) == 0)
+                {
+                    lblMultipli.Content = $"{i / a}/{d}";
+                }
+                ProgressBar.Value = i / d;
+            }
+        }
+    }
+}
